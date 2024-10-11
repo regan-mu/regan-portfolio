d2fe2c21560522c074f8aa28d78be4242816a016
regan-mu <regansomi@gmail.com>
2024-10-11 12:50:38 +0300

    Fix articles overflow in mobile screens

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { ContextWrapper } from "./context/AppContext";
 
 export const metadata: Metadata = {
   title: "Regan Muthomi",
-  description: "Software with years of experience building scalable software solutions at a high level.",
+  description: "Software with over 3 years of experience building scalable software solutions at a high level.",
 };
 
 export default function RootLayout({
